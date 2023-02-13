1f0cfbcac163cf2e5c0ad81a35551bf6a8618138
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2023-02-13 06:58:46 +0000

    upd

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef vector<int> num;
-num add(num a,num b,int conv){
-    num ans;
-    
+void upd(num &in, int conv) {
+  int p=0;
+  for (int &i : in) {
+    p++;
+    if (i > conv) {
+      if(p==in.size()){
+        in.resize(in.size()+1);
+      }
+      in[p]+=i/conv;
+      in[p-1]%=conv;
+    }
+  }
 }
-int main(){
-
+num add(num a, num b, int conv) { 
+  num ans; 
+  for(int i=0;i<max(a.size(),b.size());i++){
+    if(a.size()>b.size()){
+      if(b.size()-1<i){
+        ans.push_back(a[i]);
+      }else{
+        ans.push_back(a[i]+b)
+      }
+    }
+  }
 }
+int main() {}
